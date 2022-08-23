53549d05174cc6cd84d64bf9dc5c0542cfee4768
Yureyko228 <60176477+Yureyko228@users.noreply.github.com>
2022-08-23 20:57:14 +0300

    update bottonBar

--- lib/bottom_bar/bottom_bar.dart
+++ lib/bottom_bar/bottom_bar.dart
@@ -3,46 +3,56 @@ import 'package:permission_handler/permission_handler.dart';
 import 'package:shum_app/main.dart';
 import 'package:shum_app/screens/home.dart';
 
-class BottomBar extends StatelessWidget {
+class BottomBar extends StatefulWidget {
   const BottomBar({Key? key}) : super(key: key);
 
   @override
+  State<BottomBar> createState() => _BottomBarState();
+}
+
+class _BottomBarState extends State<BottomBar> {
+  String? thisRoute() => ModalRoute.of(context)?.settings.name;
+
+  Color activeBtnColor(String route) {
+    return route == thisRoute()
+        ? const Color.fromARGB(255, 240, 236, 236)
+        : Colors.transparent;
+  }
+
+  String? activeButtonNavigation(String route) {
+    thisRoute() != route ? Navigator.of(context).pushNamed(route) : null;
+  }
+
+  bool? getIsRecording() {
+    setState(() {});
+    return context.findAncestorStateOfType<HomeState>()?.isRecording ?? false;
+  }
+
+  getOnStart() {
+    return context.findAncestorStateOfType<HomeState>()?.start();
+  }
+
+  getOnStop() {
+    return context.findAncestorStateOfType<HomeState>()?.stop();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    String? thisRoute() => ModalRoute.of(context)?.settings.name;
-
-    Color activeBtnColor(String route) {
-      return route == thisRoute()
-          ? const Color.fromARGB(255, 240, 236, 236)
-          : Colors.transparent;
-    }
-
-    bool? getIsRecording() =>
-        context.findAncestorStateOfType<HomeState>()?.isRecording;
-    getOnStart() {
-      return context.findAncestorStateOfType<HomeState>()?.start();
-    }
-
-    getOnStop() {
-      return context.findAncestorStateOfType<HomeState>()?.stop();
-    }
-
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       children: [
         Material(
-          borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+          borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
           color: activeBtnColor(AllRoutes.saves),
           child: InkWell(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+            borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
             onTap: () {
-              thisRoute() != AllRoutes.saves
-                  ? Navigator.of(context).pushNamed(AllRoutes.saves)
-                  : null;
+              activeButtonNavigation(AllRoutes.saves);
             },
             child: SizedBox(
               width: MediaQuery.of(context).size.width / 4,
               height: 75,
-              child: Icon(
+              child: const Icon(
                 Icons.save,
                 size: 30,
                 color: Colors.grey,
@@ -52,13 +62,11 @@ class BottomBar extends StatelessWidget {
         ),
         Material(
           color: activeBtnColor(AllRoutes.home),
-          borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+          borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
           child: InkWell(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+            borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
             onTap: () async {
-              thisRoute() != AllRoutes.home
-                  ? Navigator.of(context).pushNamed(AllRoutes.home)
-                  : null;
+              activeButtonNavigation(AllRoutes.home);
 
               var status = await Permission.microphone.isDenied;
 
@@ -75,23 +83,25 @@ class BottomBar extends StatelessWidget {
             child: SizedBox(
               width: MediaQuery.of(context).size.width / 4,
               height: 75,
-              child: Icon(
-                Icons.mic,
-                size: 30,
-                color: Colors.grey,
-              ),
+              child: getIsRecording()!
+                  ? const Icon(Icons.pause)
+                  : const Icon(
+                      Icons.mic,
+                      size: 30,
+                      color: Colors.green,
+                    ),
             ),
           ),
         ),
         Material(
-          borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+          borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
           child: InkWell(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+            borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
             onTap: () {},
             child: SizedBox(
               width: MediaQuery.of(context).size.width / 4,
               height: 75,
-              child: Icon(
+              child: const Icon(
                 Icons.menu,
                 size: 30,
                 color: Colors.grey,
@@ -100,14 +110,14 @@ class BottomBar extends StatelessWidget {
           ),
         ),
         Material(
-          borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+          borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
           child: InkWell(
-            borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
+            borderRadius: const BorderRadius.vertical(top: Radius.circular(20)),
             onTap: () {},
             child: SizedBox(
               width: MediaQuery.of(context).size.width / 4,
               height: 75,
-              child: Icon(
+              child: const Icon(
                 Icons.timer_outlined,
                 size: 30,
                 color: Colors.grey,
